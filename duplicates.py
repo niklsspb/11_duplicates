baa6a10a0f4212e3d3b1820164b1035aa185b16a
niklsspb <prof051@yandex.ru>
2016-12-21 21:55:34 +0300

    Update duplicates.py
    Версия #0.02

--- duplicates.py
+++ duplicates.py
@@ -13,6 +13,7 @@ def fill_list(file_path):
 
 
 def are_files_duplicates(dir_list_one, dir_list_two):
+    duplicate = []
     for f1 in dir_list_one:
         (dirname_list_one, fname_list_one) = os.path.split(f1)
         size_list_one = os.path.getsize(os.path.join(dirname_list_one, fname_list_one))
@@ -21,21 +22,29 @@ def are_files_duplicates(dir_list_one, dir_list_two):
                 (dirname_list_two, fname_list_two) = os.path.split(f2)
                 size_list_two = os.path.getsize(os.path.join(dirname_list_two, fname_list_two))
                 if fname_list_one == fname_list_two and size_list_one == size_list_two:
-                    try:
-                        full_name_one = os.path.join(dirname_list_one, fname_list_one)
-                        full_name_two = os.path.join(dirname_list_two, fname_list_two)
-                        print("Find duplicate dir_one = {0} where_delete_dir = {1} ".format(full_name_one, full_name_two))
-                    except PermissionError:
-                        pass
-                    finally:
-                        os.chmod(full_name_two, 0o666)
-                        os.remove(full_name_two)
-                        print("Duplicate removed {0}".format(full_name_two))
+                    full_name_two = os.path.join(dirname_list_two, fname_list_two)
+                    duplicate.append(full_name_two)
             except FileNotFoundError:
                 pass
+    return duplicate
 
+
+def remove_duplicate(file):
+    try:
+        print(file)
+        for f in file:
+            os.chmod(f, 0o666)
+            os.remove(f)
+            print("Дубликат удален который хранился по следующему пути = {0}".format(f))
+
+    except PermissionError:
+        print("PermissionError")
+        print("Try chmod 666")
 
 if __name__ == '__main__':
     folder_one = input()
     folder_two = input()
-    are_files_duplicates(fill_list(folder_one), fill_list(folder_two))
+    files_in_folder_one = fill_list(folder_one)
+    files_in_folder_two = fill_list(folder_two)
+    duplicates = are_files_duplicates(files_in_folder_one, files_in_folder_two)
+    remove_duplicate(duplicates)
